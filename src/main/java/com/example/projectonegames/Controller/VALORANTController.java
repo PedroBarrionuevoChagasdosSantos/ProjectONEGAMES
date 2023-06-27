99270937bcf275602e64f2aff2239316ccdcfed3
PedroBarrionuevoChagasdosSantos <pedrinhobcs@gmail.com>
2023-06-27 20:45:20 -0300

    ProjectOnegames
    Tentando mexer nas noticias

--- src/main/java/com/example/projectonegames/Controller/VALORANTController.java
+++ src/main/java/com/example/projectonegames/Controller/VALORANTController.java
@@ -1,13 +1,14 @@
 package com.example.projectonegames.Controller;
 
+import com.example.projectonegames.ConnectionSingleton;
 import com.example.projectonegames.HelloApplication;
+import com.example.projectonegames.Model.Jogos;
 import javafx.fxml.FXML;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class VALORANTController {
 
@@ -28,6 +29,22 @@ public class VALORANTController {
 
             ResultSet resultado = connection.createStatement().executeQuery(sql);
 
+    }
+    public List<Jogos> getAll() throws SQLException {
+
+        try(PreparedStatement preparedStatement = ConnectionSingleton.getConnection().prepareStatement("select Titulo AS titulo, Texto AS Texto, Autor AS Autor from noticias order by Jogos_Jogos_id ASC;");
+            ResultSet resultSet = preparedStatement.executeQuery()){
+
+            List<Jogos> jogos = new ArrayList<>();
+            while (resultSet.next()){
+                Jogos jogos = new Jogos();
+                jogos.classificação = resultSet.getInt(1);
+                jogos.nome = resultSet.getString(2);
+                jogos.categoria = resultSet.getString(3);
+                jogos.add(jogos);
+            }
+            return jogos;
+        }
     }
 
 }
