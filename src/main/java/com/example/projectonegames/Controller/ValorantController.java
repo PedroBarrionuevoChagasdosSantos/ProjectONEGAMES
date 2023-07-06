a02260ad37cf449d4f441dfd73367ed714c79430
Pedro Barrionuevo Chagas dos Santos <pedrinhobcs@gmail.com>
2023-07-06 15:56:32 -0300

    updateee 06/07

--- src/main/java/com/example/projectonegames/Controller/ValorantController.java
+++ src/main/java/com/example/projectonegames/Controller/ValorantController.java
@@ -1,57 +1,43 @@
 package com.example.projectonegames.Controller;
 
+import com.example.projectonegames.HelloApplication;
+import javafx.application.Application;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.text.TextAlignment;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import static javafx.application.Application.launch;
+import java.io.IOException;
 
 
-public class ValorantController implements Initializable, ValorantController1 {
-    //COLOCAR OS fxml QUE TEM NA TELA DO SCENEBUILDER
+public class ValorantController extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Sei la oq aqui vai ter que estar o texto se n aparecer como quebra linha é pq n funcionou" +
+                "ent mlhr dar certo pq falta pouco");
+
+        label.setWrapText(true);
+
+        ScrollPane root = new ScrollPane();
+        root.getChildrenUnmodifiable().add(label);
+
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Valorant noticias1");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @FXML
-    ScrollPane labelTitulo;
-
-    @FXML
-    ScrollPane labelTexto;
-
-    @FXML
-    ScrollPane labelData;
-
-
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
-        public void start (Stage Stage stage;
-        stage){
-            Scene scene = new Scene(new Group());
-            stage.setTitle("Label Sample");
-            stage.setWidth(400);
-            stage.setHeight(180);
-
-            HBox hbox = new HBox();
-
-            Label label1 = new Label("Search long long long long long long long long long ");
-            label1.setPrefWidth(100);
-            label1.setWrapText(true);
-
-            hbox.setSpacing(10);
-            hbox.getChildren().add((label1));
-            ((Group) scene.getRoot()).getChildren().add(hbox);
-
-            stage.setScene(scene);
-            stage.show();
-        }
-
+    public void Voltar() throws IOException {
+        HelloApplication.setRoot("home-view");
     }
 }
 
