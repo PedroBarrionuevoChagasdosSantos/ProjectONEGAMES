e062c3db77e67857f41651606000d9803dd263b1
Pedro Barrionuevo Chagas dos Santos <pedrinhobcs@gmail.com>
2023-07-11 14:59:15 -0300

    updateeeee e

--- src/main/java/com/example/projectonegames/Controller/ValorantController.java
+++ src/main/java/com/example/projectonegames/Controller/ValorantController.java
@@ -10,28 +10,65 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-
 public class ValorantController implements Initializable {
 
     @FXML
     Label labelTexto;
+    @FXML
+    Label labelTitulo;
+    @FXML
+    Label labelData;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         ValorantDAO valorantDAO = new ValorantDAO();
         try {
-            String texto = valorantDAO.valorant();
+            String texto = valorantDAO.valorant1();
 
             labelTexto.setWrapText(true);
             labelTexto.setText(texto);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            String titulo = valorantDAO.titulo1();
+
+            labelTitulo.setWrapText(true);
+            labelTitulo.setText(titulo);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            String data = valorantDAO.data1();
+
+            labelData.setWrapText(true);
+            labelData.setText(data);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            String titulo = valorantDAO.titulo1();
+
+            labelTitulo.setWrapText(true);
+            labelTitulo.setText(titulo);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
 
 
-        }
+
+
+
+
+
+
+
+    }
 
 
     @FXML
--- src/main/java/com/example/projectonegames/Model/ValorantDAO.java
+++ src/main/java/com/example/projectonegames/Model/ValorantDAO.java
@@ -6,7 +6,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ValorantDAO {
-    public String valorant() throws SQLException {
+
+    public String titulo1() throws SQLException {
+
+        try(PreparedStatement preparedStatement = ConnectionSingleton.getConnection().prepareStatement("select Titulo from noticias where Noticias_id =  1;");
+            ResultSet resultSet = preparedStatement.executeQuery()) {
+            resultSet.next();
+
+            String titulo = resultSet.getString(1);
+
+            return titulo;
+        }
+    }
+    public String valorant1() throws SQLException {
 
         try (PreparedStatement preparedStatement = ConnectionSingleton.getConnection().prepareStatement("select texto from noticias where Noticias_id =  1;");
              ResultSet resultSet = preparedStatement.executeQuery()) {
@@ -15,6 +27,31 @@ public class ValorantDAO {
             String texto = resultSet.getString(1);
 
             return texto;
+        }
+    }
+    public String data1() throws SQLException {
+
+        try (PreparedStatement preparedStatement = ConnectionSingleton.getConnection().prepareStatement("select Data from noticias where Noticias_id =  1;");
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            resultSet.next();
+
+            String data = resultSet.getString(1);
+
+            return data;
+        }
+    }
+
+
+
+    public String titulo2() throws SQLException {
+
+        try(PreparedStatement preparedStatement = ConnectionSingleton.getConnection().prepareStatement("select Titulo from noticias where Noticias_id =  2;");
+            ResultSet resultSet = preparedStatement.executeQuery()) {
+            resultSet.next();
+
+            String titulo2 = resultSet.getString(1);
+
+            return titulo2;
         }
     }
 }
